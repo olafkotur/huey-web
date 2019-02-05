afb2da2e7d072a1dfae0448e3babe820ea0cf5fd
olafkotur <olafkotur97@gmail.com>
2019-02-05 20:11:36 +0000

    Basic nav

--- src/App.js
+++ src/App.js
@@ -1,28 +1,94 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { createNavigator, SwitchRouter, SceneView } from "@react-navigation/core";
+import { createBrowserApp, Link } from "@react-navigation/web";
+window.__DEV__ = true;
 
-class App extends Component {
-  render() {
+function About() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div>
+            <h2>About Screen</h2>
+        </div>
     );
-  }
 }
+About.path = "";
+About.navigationOptions = {
+    title: "About",
+    linkName: "About Page"
+};
+
+function Home() {
+    return (
+        <div>
+            <h2>Home Screen</h2>
+            <Link routeName="Profile" params={{ name: "brent" }}>Go to Brent's Profile</Link>
+        </div>
+    );
+}
+Home.path = "";
+Home.navigationOptions = {
+    title: "Home",
+    linkName: "Home Page"
+};
+
+const Profile = ({ navigation }) => (
+    <div>
+        <h2>{navigation.getParam("name")}'s Profile</h2>
+        <Link routeName="Home">Go Home</Link>
+    </div>
+);
+Profile.path = "person/:name";
+Profile.navigationOptions = ({ navigation }) => ({
+title: navigation.getParam("name"),
+linkName: "Profile Page"
+});
+
+class SidebarView extends React.Component {
+render() {
+const { descriptors, navigation } = this.props;
+const activeKey = navigation.state.routes[navigation.state.index].key;
+const descriptor = descriptors[activeKey];
+return (
+<div
+style={{ display: "flex", height: "100%", justifyContent: "stretch" }}
+>
+<div
+style={{
+width: 300,
+backgroundColor: "#efefef",
+borderRight: "1px solid #99b"
+}}
+>
+<h1>Hello, Navigation</h1>
+<Link routeName="Home">Home</Link>
+<Link routeName="About">About</Link>
+<Link routeName="Profile" params={{ name: "jamie" }}>
+About Jamie
+</Link>
+<Link routeName="Profile" params={{ name: "brent" }}>
+About Brent
+</Link>
+</div>
+<div>
+<SceneView
+component={descriptor.getComponent()}
+navigation={descriptor.navigation}
+/>
+</div>
+</div>
+);
+}
+}
+
+const AppNavigator = createNavigator(
+SidebarView,
+SwitchRouter({
+Home,
+About,
+Profile
+}),
+{}
+);
+
+const App = createBrowserApp(AppNavigator);
 
 export default App;
