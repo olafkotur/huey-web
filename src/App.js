04b88653d88358c337445c69091048e7f0c1b6dc
olafkotur <olafkotur97@gmail.com>
2019-02-05 20:17:53 +0000

    More nav

--- src/App.js
+++ src/App.js
@@ -3,31 +3,30 @@ import { createNavigator, SwitchRouter, SceneView } from "@react-navigation/core
 import { createBrowserApp, Link } from "@react-navigation/web";
 window.__DEV__ = true;
 
-function About() {
+function Login() {
     return (
         <div>
-            <h2>About Screen</h2>
+            <h2>Login Screen</h2>
         </div>
     );
 }
-About.path = "";
-About.navigationOptions = {
-    title: "About",
-    linkName: "About Page"
+Login.path = "";
+Login.navigationOptions = {
+    title: "Login",
+    linkName: "Login Page"
 };
 
-function Home() {
+function Admin() {
     return (
         <div>
             <h2>Home Screen</h2>
-            <Link routeName="Profile" params={{ name: "brent" }}>Go to Brent's Profile</Link>
         </div>
     );
 }
-Home.path = "";
-Home.navigationOptions = {
-    title: "Home",
-    linkName: "Home Page"
+Admin.path = "";
+Admin.navigationOptions = {
+    title: "Admin",
+    linkName: "Admin Page"
 };
 
 const Profile = ({ navigation }) => (
@@ -38,55 +37,37 @@ const Profile = ({ navigation }) => (
 );
 Profile.path = "person/:name";
 Profile.navigationOptions = ({ navigation }) => ({
-title: navigation.getParam("name"),
-linkName: "Profile Page"
+    title: navigation.getParam("name"),
+    linkName: "Profile Page"
 });
 
 class SidebarView extends React.Component {
-render() {
-const { descriptors, navigation } = this.props;
-const activeKey = navigation.state.routes[navigation.state.index].key;
-const descriptor = descriptors[activeKey];
-return (
-<div
-style={{ display: "flex", height: "100%", justifyContent: "stretch" }}
->
-<div
-style={{
-width: 300,
-backgroundColor: "#efefef",
-borderRight: "1px solid #99b"
-}}
->
-<h1>Hello, Navigation</h1>
-<Link routeName="Home">Home</Link>
-<Link routeName="About">About</Link>
-<Link routeName="Profile" params={{ name: "jamie" }}>
-About Jamie
-</Link>
-<Link routeName="Profile" params={{ name: "brent" }}>
-About Brent
-</Link>
-</div>
-<div>
-<SceneView
-component={descriptor.getComponent()}
-navigation={descriptor.navigation}
-/>
-</div>
-</div>
-);
-}
+    render() {
+        const { descriptors, navigation } = this.props;
+        const activeKey = navigation.state.routes[navigation.state.index].key;
+        const descriptor = descriptors[activeKey];
+        return (
+            <div style={{ display: "flex", height: "100%", justifyContent: "stretch" }}>
+                <div style={{width: 300, backgroundColor: "#efefef", borderRight: "1px solid #99b"}}>
+                    <h1>Navigation</h1>
+                    <Link routeName="Login">Login</Link>
+                    <br/><br/>
+                    <Link routeName="Admin">Admin</Link>
+                </div>
+                <div>
+                    <SceneView component={descriptor.getComponent()} navigation={descriptor.navigation}/>
+                </div>
+            </div>
+        );
+    }
 }
 
 const AppNavigator = createNavigator(
-SidebarView,
-SwitchRouter({
-Home,
-About,
-Profile
-}),
-{}
+    SidebarView,
+    SwitchRouter({
+        Login,
+        Admin,
+    }), {}
 );
 
 const App = createBrowserApp(AppNavigator);
