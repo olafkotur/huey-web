ce6115b340ca5f0b95378481bb98d3ee2203ca69
olafkotur <olafkotur97@gmail.com>
2019-02-05 20:22:44 +0000

    Made it sexy clean af

--- src/App.js
+++ src/App.js
@@ -1,45 +1,9 @@
 import React from "react";
 import { createNavigator, SwitchRouter, SceneView } from "@react-navigation/core";
 import { createBrowserApp, Link } from "@react-navigation/web";
-window.__DEV__ = true;
 
-function Login() {
-    return (
-        <div>
-            <h2>Login Screen</h2>
-        </div>
-    );
-}
-Login.path = "";
-Login.navigationOptions = {
-    title: "Login",
-    linkName: "Login Page"
-};
-
-function Admin() {
-    return (
-        <div>
-            <h2>Home Screen</h2>
-        </div>
-    );
-}
-Admin.path = "";
-Admin.navigationOptions = {
-    title: "Admin",
-    linkName: "Admin Page"
-};
-
-const Profile = ({ navigation }) => (
-    <div>
-        <h2>{navigation.getParam("name")}'s Profile</h2>
-        <Link routeName="Home">Go Home</Link>
-    </div>
-);
-Profile.path = "person/:name";
-Profile.navigationOptions = ({ navigation }) => ({
-    title: navigation.getParam("name"),
-    linkName: "Profile Page"
-});
+import Admin from './views/Admin';
+import Login from './views/Login';
 
 class SidebarView extends React.Component {
     render() {
@@ -51,7 +15,6 @@ class SidebarView extends React.Component {
                 <div style={{width: 300, backgroundColor: "#efefef", borderRight: "1px solid #99b"}}>
                     <h1>Navigation</h1>
                     <Link routeName="Login">Login</Link>
-                    <br/><br/>
                     <Link routeName="Admin">Admin</Link>
                 </div>
                 <div>
---
+++ src/views/Admin.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Admin extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>ADMINNNN</h1>
+			</div>
+		);
+	}
+}
+
+export default Admin;
