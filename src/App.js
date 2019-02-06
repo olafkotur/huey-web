3a42388f5d639ad0f88d6c9327f34a72cbec75b7
olafkotur <olafkotur97@gmail.com>
2019-02-06 15:38:37 +0000

    Added login page, checks with firebase if account is an admin then logs in

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import React from "react";
 import { createNavigator, SwitchRouter, SceneView } from "@react-navigation/core";
 import { createBrowserApp, Link } from "@react-navigation/web";
 
+import { initialize } from './Firebase';
 import Admin from './views/Admin';
 import Login from './views/Login';
 
 class SidebarView extends React.Component {
+
+    componentDidMount = () => {
+        initialize();
+    }
+
     render() {
         const { descriptors, navigation } = this.props;
         const activeKey = navigation.state.routes[navigation.state.index].key;
