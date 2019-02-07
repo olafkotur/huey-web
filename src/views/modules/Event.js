92e7b43950d6284625d50967eb89dbb39bb40225
George Park <g.park1@lancaster.ac.uk>
2019-02-07 22:39:56 +0000

    Styled header for events screen, added card styling for events

--- src/views/modules/Event.js
+++ src/views/modules/Event.js
@@ -4,13 +4,17 @@ export default class Event extends React.Component {
 	render() {
 		return (
 			<div class="event-container">
-				<div class="row-left">
-					<h3>{this.props.name}</h3>
-					<p>Organisers: {this.props.organisers}</p>
-					<p>Protestors: {this.props.protestors}</p>
-				</div>
-				<div class="row-right">
-					<img class='qr-code' src='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/QR_code_for_mobile_English_Wikipedia.svg/1024px-QR_code_for_mobile_English_Wikipedia.svg.png' alt="QR"></img>
+				<div class="single-event">
+					<div class="card-topbar">
+						<h3 class="card-title">{this.props.name}</h3>
+					</div>
+					<div class="qr-div">
+						<img class='qr-code' src='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/QR_code_for_mobile_English_Wikipedia.svg/1024px-QR_code_for_mobile_English_Wikipedia.svg.png' alt="QR"></img>
+					</div>
+					<div class="event-text-container">
+						<p>Organisers: {this.props.organisers}</p>
+						<p>Protestors: {this.props.protestors}</p>
+					</div>
 				</div>
 			</div>
 		);
