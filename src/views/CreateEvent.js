5575718b494b73c638911ef1635c54664f19fda5
George Park <g.park1@lancaster.ac.uk>
2019-02-12 15:22:57 +0000

    added some styling to create events page

--- src/views/CreateEvent.js
+++ src/views/CreateEvent.js
@@ -25,34 +25,36 @@ class CreateEvent extends React.Component {
 
 	render() {
 		return (
-			<div>
-				<h1>Create Event</h1>
+			<div class="events-big-div">
+				<h1 class="scene-header-text">Create Event</h1>
 
-				<label>Event Name:</label>
-				<br/>
-				<input id="name" type="text" onChange={(name) => this.setState({name: name.target.value})}/>
+				<div class="create-event-form">
+					<label>Event Name:</label>
+					<br/>
+					<input id="name" type="text" onChange={(name) => this.setState({name: name.target.value})}/>
 
-				<br/><br/>
-				
-				<label>Number of Organisers:</label>
-				<br/>
-				<input id="organisers" type="number" onChange={(organisers) => this.setState({organisers: organisers.target.value})}/>
+					<br/><br/>
+					
+					<label>Number of Organisers:</label>
+					<br/>
+					<input id="organisers" type="number" onChange={(organisers) => this.setState({organisers: organisers.target.value})}/>
 
-				<br/><br/>
+					<br/><br/>
 
-				<label>Number of Protestors:</label>
-				<br/>
-				<input id="protestors" type="number" onChange={(protestors) => this.setState({protestors: protestors.target.value})}/>
+					<label>Number of Protestors:</label>
+					<br/>
+					<input id="protestors" type="number" onChange={(protestors) => this.setState({protestors: protestors.target.value})}/>
 
-				<br/><br/>
+					<br/><br/>
 
-				<label>Protest Info</label>
-				<br/>
-				<textarea id="protestInfo" type="text" onChange={(protestInfo) => this.setState({protestInfo: protestInfo.target.value})}/>
-				
-				<br/><br/>
+					<label>Protest Info</label>
+					<br/>
+					<textarea id="protestInfo" type="text" onChange={(protestInfo) => this.setState({protestInfo: protestInfo.target.value})}/>
+					
+					<br/><br/>
 
-				<button onClick={this.handleCreateEvent}>Create</button>
+					<button class="header-buttons" onClick={this.handleCreateEvent}>Create</button>
+				</div>
 			</div>
 		);
 	}
